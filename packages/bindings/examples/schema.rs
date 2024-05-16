438d076ddb337be3119c72b36c2243f6641e07c5
expertdicer <expertdicer@gmail.com>
2024-05-16 14:38:47 +0700

    update crates name and dependencies

--- packages/bindings/examples/schema.rs
+++ packages/bindings/examples/schema.rs
@@ -3,7 +3,7 @@ use std::fs::create_dir_all;
 
 use cosmwasm_schema::{export_schema, remove_schemas, schema_for};
 
-use classic_bindings::{
+use classic_bindings_genuine::{
     ExchangeRateItem, ExchangeRatesResponse, SwapResponse, TaxCapResponse,
     TaxRateResponse, TerraQuery, TerraMsg
 };
